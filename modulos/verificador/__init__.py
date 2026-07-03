1d7a964ed7e9e4cb9d6617bc2d1ef3211202a89d
David Felipe <david.dfcsb9@gmail.com>
2026-07-03 15:00:59 -0300

    Adicionei novos verificadores

--- modulos/verificador/__init__.py
+++ modulos/verificador/__init__.py
@@ -1,5 +1,9 @@
+from modulos.texto import *
+
+texto = Texto()
+
 class Verificadores:
-    def verifica_bool(txt):
+    def verifica_bool(self, txt):
         while True:
             label = input(txt).strip().lower()
 
@@ -12,4 +16,44 @@ class Verificadores:
                 return True
             
             return False
+        
 
+    def verifica_nums_naturais(self, txt):
+        while True:
+            label = input(txt).strip()
+
+            try:
+                num = int(label)
+
+                if num < 0:
+                    print(f'{texto.cores('vermelho')}[ERROR] Digite apenas números maiores que {texto.cores()}{texto.cores('amarelo')}0{texto.cores()}{texto.cores('vermelho')}!{texto.cores()}\n')
+                    continue
+
+                return num
+
+            except (TypeError, ValueError):
+                print(f'{texto.cores('vermelho')}[ERROR] Digite apenas números naturais!\n{texto.cores()}')
+
+    def verifica_int(self, txt):
+        while True:
+            label = input(txt).strip()
+
+            try:
+                num = int(label)
+
+                return num
+
+            except (TypeError, ValueError):
+                print(f'{texto.cores('vermelho')}[ERROR] Digite apenas números inteiros!\n{texto.cores()}')
+    
+    def verifica_float(self, txt):
+        while True:
+            label = input(txt).strip().replace(',','.')
+
+            try:
+                num = int(label)
+
+                return num
+
+            except (TypeError, ValueError):
+                print(f'{texto.cores('vermelho')}[ERROR] Digite apenas números!\n{texto.cores()}')
